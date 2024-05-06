18c07c25aba0ce51dd4ca52b3c0db5e9cd6f7411
andrey F de castro <andreycastro.564@gmail.com>
2024-05-06 15:48:59 -0300

    feat: criação das database e das tabelas

---
+++ db/script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE anime_characters (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+power VARCHAR(100) NOT NULL,
+level INT NOT NULL,
+hp INT NOT NULL
+);
+
+CREATE TABLE battles (
+id SERIAL PRIMARY KEY,
+character1_id INT REFERENCES anime_characters(id),
+character2_id INT REFERENCES anime_characters(id),
+winner_id INT REFERENCES anime_characters(id)
+);
